bff8de2b4ee27b3420916b3ca8ed7c66d179d62f
searKing <searKingChan@gmail.com>
2020-02-19 22:20:54 +0800

    jest with async

--- __tests__/markdown2htmlPro-spec.ts
+++ __tests__/markdown2htmlPro-spec.ts
@@ -9,33 +9,33 @@ const taskList: string = `#### GFM task list
 - [ ] GFM task list 4
     - [ ] GFM task list 4-1
     - [ ] GFM task list 4-2`;
-const taskListExpect: string = `<h4>GFM task list</h4>
-<ul class=\"contains-task-list\">
-<li class=\"task-list-item enabled\"><label><input class=\"task-list-item-checkbox\" checked=\"\"type=\"checkbox\"> GFM task list 1</label></li>
-<li class=\"task-list-item enabled\"><label><input class=\"task-list-item-checkbox\" checked=\"\"type=\"checkbox\"> GFM task list 2</label></li>
-<li class=\"task-list-item enabled\"><label><input class=\"task-list-item-checkbox\"type=\"checkbox\"> GFM task list 3</label>
-<ul class=\"contains-task-list\">
-<li class=\"task-list-item enabled\"><label><input class=\"task-list-item-checkbox\"type=\"checkbox\"> GFM task list 3-1</label></li>
-<li class=\"task-list-item enabled\"><label><input class=\"task-list-item-checkbox\"type=\"checkbox\"> GFM task list 3-2</label></li>
-<li class=\"task-list-item enabled\"><label><input class=\"task-list-item-checkbox\"type=\"checkbox\"> GFM task list 3-3</label></li>
+const taskListExpect: string = `<h4 id="gfm-task-list">GFM task list</h4>
+<ul class="contains-task-list">
+<li class="task-list-item enabled"><label><input class="task-list-item-checkbox" checked=""type="checkbox"> GFM task list 1</label></li>
+<li class="task-list-item enabled"><label><input class="task-list-item-checkbox" checked=""type="checkbox"> GFM task list 2</label></li>
+<li class="task-list-item enabled"><label><input class="task-list-item-checkbox"type="checkbox"> GFM task list 3</label>
+<ul class="contains-task-list">
+<li class="task-list-item enabled"><label><input class="task-list-item-checkbox"type="checkbox"> GFM task list 3-1</label></li>
+<li class="task-list-item enabled"><label><input class="task-list-item-checkbox"type="checkbox"> GFM task list 3-2</label></li>
+<li class="task-list-item enabled"><label><input class="task-list-item-checkbox"type="checkbox"> GFM task list 3-3</label></li>
 </ul>
 </li>
-<li class=\"task-list-item enabled\"><label><input class=\"task-list-item-checkbox\"type=\"checkbox\"> GFM task list 4</label>
-<ul class=\"contains-task-list\">
-<li class=\"task-list-item enabled\"><label><input class=\"task-list-item-checkbox\"type=\"checkbox\"> GFM task list 4-1</label></li>
-<li class=\"task-list-item enabled\"><label><input class=\"task-list-item-checkbox\"type=\"checkbox\"> GFM task list 4-2</label></li>
+<li class="task-list-item enabled"><label><input class="task-list-item-checkbox"type="checkbox"> GFM task list 4</label>
+<ul class="contains-task-list">
+<li class="task-list-item enabled"><label><input class="task-list-item-checkbox"type="checkbox"> GFM task list 4-1</label></li>
+<li class="task-list-item enabled"><label><input class="task-list-item-checkbox"type="checkbox"> GFM task list 4-2</label></li>
 </ul>
 </li>
 </ul>
 `;
-(async () => {
+
+test('Should greet with message', async () => {
   const markdown2htmlPro = new Markdown2HtmlPro();
   const taskListActual: string = await markdown2htmlPro.markdown2html(taskList);
-
-  test('Should greet with message', () => {
-    expect(taskListActual).toBe(taskListExpect);
-  });
-})();
+  // console.log('taskListActual= ', taskListActual);
+  // console.log('taskListExpect= ', taskListExpect);
+  expect(taskListActual).toBe(taskListExpect);
+});
 
 test('Should have Markdown2HtmlPro available', () => {
   expect(Markdown2HtmlPro).toBeTruthy();
